013642bbcaf4499503db33260d6e0fc57a8a2d69
Karl Heinz Marbaise <khmarbaise@apache.org>
2021-10-08 15:12:11 +0200

    Fixed unused import.

--- maven-core/src/main/java/org/apache/maven/DefaultMaven.java
+++ maven-core/src/main/java/org/apache/maven/DefaultMaven.java
@@ -30,7 +30,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Stream;
